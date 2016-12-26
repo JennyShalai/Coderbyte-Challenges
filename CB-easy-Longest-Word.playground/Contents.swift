863dd50d63c5de4b85d2270b31634dd5e3d70609
JennyShalai <jenny.shalai@gmail.com>
2016-12-26 14:13:21 -0500

    longest word in string with regular expression

--- CB-easy-Longest-Word.playground/Contents.swift
+++ CB-easy-Longest-Word.playground/Contents.swift
@@ -30,4 +30,38 @@ func LongestWord(sen: String) -> String {
     
 }
 
-print(LongestWord(sen: str))    // playground
+func LongestWordWithRegex(sen: String) -> String {
+    
+    let regexPattern = "[A-Za-z ]"
+    var filteredString = ""
+    
+    do {
+        let regex = try NSRegularExpression(pattern: regexPattern, options: [])
+        let matches = regex.matches(in: str, options: [], range: NSRange(location: 0, length: str.characters.count))
+        
+        for match in matches {
+            let charIndex = match.rangeAt(0).location
+            filteredString += String(str[str.index(str.startIndex, offsetBy: charIndex)])
+        }
+    } catch let error as NSError {
+        print(error.localizedDescription)
+    }
+    
+    let words = filteredString.components(separatedBy: " ")
+    
+    var counter = 0
+    var longest = ""
+    
+    for word in words {
+        if word.characters.count > counter {
+            longest = word
+            counter = word.characters.count
+        }
+        
+    }
+    return longest
+    
+}
+
+print(LongestWord(sen: str))            // playground
+print(LongestWordWithRegex(sen: str))   // playground
